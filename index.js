da3d5cb00323a258635ccd15b52cfe89134cc627
Muhamad Alwan Fadhlurrohman <alwan@MacBook-Air-Muhamad.local>
2025-07-25 17:33:35 +0700

    change variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const apiData = require("./objectFarid");
+const dataAnime = require("./objectFarid");
 // console.log("Data dari data.js:", apiData )
 const PORT = process.env.PORT || 3000;
 
