240f23ff4fd5679b430104a30587719438c8bb4f
Muhamad Alwan Fadhlurrohman <alwan@MacBook-Air-Muhamad.local>
2025-07-25 18:44:50 +0700

    fix: use process.env.PORT for Render

--- index.js
+++ index.js
@@ -5,29 +5,29 @@ const app = express();
 
 const apiData = require("./objectFarid");
 // console.log("Data dari objectFarid.js:", apiData)
-const PORT = process.env.PORT || 3000;
 
 app.use(cors()); // agar bisa diakses dari frontend
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send("API Galeri Anime, Created by Farid");
+    res.send("API Galeri Anime, Created by Farid");
 });
 
 app.get('/api/anime', (req, res) => {
-  res.json(apiData.dataAnime.anime);
+    res.json(apiData.dataAnime.anime);
 });
 
 app.get('/api/anime/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const anime = apiData.dataAnime.anime.find(item => item.id === id);
-
-  if (!anime) {
-    return res.status(404).json({ message: "Anime tidak ditemukan" });
-  }
-
-  res.json(anime);
+    const id = parseInt(req.params.id);
+    const anime = apiData.dataAnime.anime.find(item => item.id === id);
+    
+    if (!anime) {
+        return res.status(404).json({ message: "Anime tidak ditemukan" });
+    }
+    
+    res.json(anime);
 });
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
